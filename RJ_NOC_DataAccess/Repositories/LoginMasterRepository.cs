6142334bdc2920df129fedd62d90b80c86e9af3f
Admin <Admin@DESKTOP-1HHR4VO>
2023-07-25 11:20:34 +0530

    Login

---
+++ RJ_NOC_DataAccess/Repositories/LoginMasterRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using RJ_NOC_Model;
+using System.Threading.Tasks;
+using RJ_NOC_DataAccess.Interface;
+using System.Data.SqlClient;
+using System.Data;
+using RJ_NOC_DataAccess.Common;
+using Newtonsoft.Json;
+using System.Net.NetworkInformation;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Reflection;
+using System.Collections;
+using System.Diagnostics.Metrics;
+
+namespace RJ_NOC_DataAccess.Repository
+{
+    public class LoginMasterRepository : ILoginMasterRepository
+    {
+        private CommonDataAccessHelper _commonHelper;
+        public LoginMasterRepository(CommonDataAccessHelper commonHelper)
+        {
+            _commonHelper = commonHelper;
+        }
+
+        public List<LoginMasterDataModel> Login(string UserName, string Password)
+        {
+            string SqlQuery = " exec USP_UsersLogin @LoginID='" + UserName + "',@Password='"+ Password + "'";
+            DataTable dataTable = new DataTable();
+            dataTable = _commonHelper.Fill_DataTable(SqlQuery, "LoginMaster.Login");
+             
+            List<LoginMasterDataModel> dataModels = new List<LoginMasterDataModel>();
+            string JsonDataTable_Data = CommonHelper.ConvertDataTable(dataTable);
+            dataModels = JsonConvert.DeserializeObject<List<LoginMasterDataModel>>(JsonDataTable_Data);
+            return dataModels;
+        }
+    }
+}
---
+++ RJ_NOC_Utility/CustomerDomain/LoginMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RJ_NOC_Model;
+using RJ_NOC_Utility.CustomerDomain.Interface;
+using RJ_NOC_DataAccess.Interface;
+
+namespace RJ_NOC_Utility.CustomerDomain
+{
+    public class LoginMaster : UtilityBase, ILoginMaster
+    {
+        public LoginMaster(IRepositories unitOfWork) : base(unitOfWork)
+        {
+        }
+        List<LoginMasterDataModel> ILoginMaster.Login(string UserName, string Password)
+        {
+            return UnitOfWork.LoginMasterRepository.Login(UserName, Password);
+        }
+    }
+}
