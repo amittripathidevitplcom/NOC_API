aaba80b38ea224f1e27731fb8ac1757fae25ce46
Rajeev Kumawat <rajeev.kumawat@corpnet.co.in>
2023-08-05 14:41:31 +0530

    #deepak

--- RJ_NOC_DataAccess/Interface/IParamedicalHospitalRepository.cs
+++ RJ_NOC_DataAccess/Interface/IParamedicalHospitalRepository.cs
@@ -8,6 +8,11 @@ namespace RJ_NOC_DataAccess.Interface
 {
     public interface IParamedicalHospitalRepository
     {
+        List<HospitalAreaValidation> GetHospitalAreaValidation();
         bool SaveData(ParamedicalHospitalDataModel request);
+        ParamedicalHospitalDataModel GetData(int hospitalId);
+        List<ParamedicalHospitalDataModel> GetDataList(int collegeId);
+        bool DeleteData(int hospitalId, int modifiedBy);
+        List<ParamedicalHospitalBedValidation> GetParamedicalHospitalBedValidation(int CollegeID, int HospitalID);
     }
 }
