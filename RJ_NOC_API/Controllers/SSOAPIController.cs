db48384634ee1ad0a85618e8aa376fdebed7e810
Bhoopendra jain <Bhoopendra jain@DESKTOP-QH3VC3J>
2023-08-16 16:48:54 +0530

    Upfate

--- RJ_NOC_API/Controllers/SSOAPIController.cs
+++ RJ_NOC_API/Controllers/SSOAPIController.cs
@@ -192,38 +192,6 @@ namespace RJ_NOC_API.Controllers
             return result;
         }
 
-        //[HttpGet("BackToSSO")]
-        //public async Task<OperationResult<List<CommonDataModel_DataTable>>> BackToSSO()
-        //{
-
-        //    var result = new OperationResult<List<CommonDataModel_DataTable>>();
-        //    try
-        //    {
-        //        var token = Request.Cookies["RAJSSO"] == null ? "" : Request.Cookies["RAJSSO"].ToString();
-        //        result.State = OperationState.Success;
-        //        if (result.Data.Count > 0)
-        //        {
-        //            result.State = OperationState.Success;
-        //            result.SuccessMessage = "Data load successfully .!";
-        //        }
-        //        else
-        //        {
-        //            result.State = OperationState.Warning;
-        //            result.SuccessMessage = "No record found.!";
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        CommonDataAccessHelper.Insert_ErrorLog("CommonFuncationController.Check_SSOIDWise_LegalEntity", ex.ToString());
-        //        result.State = OperationState.Error;
-        //        result.ErrorMessage = ex.Message.ToString();
-        //    }
-        //    finally
-        //    {
-        //        // UnitOfWork.Dispose();
-        //    }
-        //    return result;
-        //}
     }
 }
 
