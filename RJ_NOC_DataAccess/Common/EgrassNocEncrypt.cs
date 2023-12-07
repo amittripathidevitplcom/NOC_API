a505be6766567976c3b0e4883747d85f1acca05a
Naresh rawat <Naresh.rawat@corpnet.co.in>
2023-12-07 15:06:53 +0530

    #RAVi

---
+++ RJ_NOC_DataAccess/Common/EgrassNocEncrypt.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ_NOC_DataAccess.Common
+{
+    public class EgrassNocEncrypt
+    {
+        public string Encrypt(string textToEncrypt, string FilePath)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 0x80;
+            rijndaelCipher.BlockSize = 0x80;
+            byte[] pwdBytes = GetFileBytes(FilePath);
+            byte[] keyBytes = new byte[0x10];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length)
+            {
+                len = keyBytes.Length;
+            }
+            Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+            ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+            byte[] plainText = Encoding.UTF8.GetBytes(textToEncrypt);
+            return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
+        }
+
+        public string Decrypt(string textToDecrypt, string FilePath)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+            rijndaelCipher.KeySize = 0x80;
+            rijndaelCipher.BlockSize = 0x80;
+            textToDecrypt = textToDecrypt.Replace(" ", "+");
+            byte[] encryptedData = Convert.FromBase64String(textToDecrypt);
+            byte[] pwdBytes = GetFileBytes(FilePath);
+            byte[] keyBytes = new byte[0x10];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length)
+            {
+                len = keyBytes.Length;
+            }
+            Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+            byte[] plainText = rijndaelCipher.CreateDecryptor().TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+            return Encoding.UTF8.GetString(plainText);
+        }
+
+        public byte[] GetFileBytes(String filePath)
+        {
+            byte[] buffer;
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            try
+            {
+                int length = (int)fileStream.Length;
+                buffer = new byte[length];
+                int count;
+                int sum = 0;
+                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                    sum += count;
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+            return buffer;
+        }
+    }
+
+    public class FinancialYear
+    {
+        int yearNumber;
+        private static readonly int firstMonthInYear = 4;
+
+        public static FinancialYear Current
+        {
+            get { return new FinancialYear(DateTime.Today); }
+        }
+
+        public FinancialYear(DateTime forDate)
+        {
+            if (forDate.Month < firstMonthInYear)
+            {
+                yearNumber = forDate.Year + 1;
+            }
+            else
+            {
+                yearNumber = forDate.Year;
+            }
+        }
+
+        public override string ToString()
+        {
+            return yearNumber.ToString();
+        }
+    }
+}
