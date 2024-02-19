5438a32f40e78750477844e86306c2f8dcb32e42
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2024-02-19 17:48:56 +0530

    updated

--- RJ_NOC_API/Controllers/PaymentController.cs
+++ RJ_NOC_API/Controllers/PaymentController.cs
@@ -923,7 +923,8 @@ namespace RJ_NOC_API.Controllers
                 result.Data.MERCHANTCODE = "15";
                 result.Data.ENCDATA = ENCDATA1;
                 result.Data.AUIN = PRN;
-                result.Data.PaymentRequestURL = "https://egras.rajasthan.gov.in/samplemerchantprelogin.aspx";
+                //result.Data.PaymentRequestURL = "https://egras.rajasthan.gov.in/samplemerchantprelogin.aspx";
+                result.Data.PaymentRequestURL = "http://10.68.69.46:62778/api/Payment/GRAS_PaymentRequest";
 
                  
 
