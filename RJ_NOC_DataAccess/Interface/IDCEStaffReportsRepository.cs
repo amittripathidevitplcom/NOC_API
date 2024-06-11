fecddad2d4b9fed888e40428fe238057e54e25bc
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2024-06-11 11:43:30 +0530

    updated

---
+++ RJ_NOC_DataAccess/Interface/IDCEStaffReportsRepository.cs
@@ -0,0 +1,20 @@
+﻿using RJ_NOC_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ_NOC_DataAccess.Interface
+{
+    public interface IDCEStaffReportsRepository
+    {
+
+
+        List<DCEStaffReportsDataModel_list> DCEStaffDetailsList(DCEStaffReportsDataModel request);
+        List<DCEStaffReports_SubjectList> GetSubjectList();
+        List<DCEStaffReports_SubjectList> GetStaffDuplicateAdharList();
+
+
+    }
+}
